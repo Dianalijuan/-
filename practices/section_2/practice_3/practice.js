59e7346e1430443e5a574036ac92819e65f03252
Dianalijuan <2803089840@qq.com>
2018-04-12 12:21:02 +0800

    collection

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,33 +1,5 @@
 'use strict';
 function count_same_elements(collection) {
-  /*//在这里写入代码
-  var result = [];
-  for (var i = 0; i < collection.length;) {
-    let count = 0;
-    for (var j = 0; j < collection.length; j++) {
-      if (collection[i] === collection[j]) {
-        count++;
-      }
-    }
-
-    if (collection[i].split("-").length > 1) {
-      result.push({
-        name: collection[i].split("-")[0],
-        summary: Number(collection[i].split("-")[1])
-      })
-    } else {
-      result.push({
-        name: collection[i],
-        summary: count
-      })
-    }
-    i += count;
-  }
-
-
-  return result;
-
-}*/
   var res = [];
 
   for(var i=0; i < collection.length; i++) {
